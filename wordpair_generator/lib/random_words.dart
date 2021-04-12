7773c5f6238c4167aec6ab4a16a59c6c2163a127
DeletedC <CurtisWoodsArt@gmail.com>
2021-04-12 14:34:33 -0400

    Added saved wordpair page, finished tutorial

--- wordpair_generator/lib/random_words.dart
+++ wordpair_generator/lib/random_words.dart
@@ -31,17 +31,55 @@ class RandomWordsState extends State<RandomWords> {
     final alreadySaved = _savedWordPairs.contains(pair);
 
     return ListTile(
-      title: Text(pair.asPascalCase, 
-        style: TextStyle(fontSize: 18)),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border, 
+      title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18)),
+      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border, 
         color: alreadySaved ? Colors.red : null),
+        onTap: () {
+          setState((){
+            if(alreadySaved) {
+              _savedWordPairs.remove(pair);
+            } else {
+              _savedWordPairs.add(pair);
+            }
+          });
+        }
         );
   }
   
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+            return ListTile(
+              title: Text(pair.asPascalCase, style: TextStyle(fontSize: 16))
+            );
+          });
+
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved WordPairs')),
+            body: ListView(children: divided));
+        } 
+      )
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('WordPair Generator')),
+      appBar: AppBar(
+        title: Text('WordPair Generator'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed:_pushSaved)
+        ]
+        ),
       body: _buildList());
   }
 }
