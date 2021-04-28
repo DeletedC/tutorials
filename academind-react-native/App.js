1cf8d3c06423b4cdc4bac9d3260c2d7f393fd2f8
DeletedC <CurtisWoodsArt@gmail.com>
2021-04-28 10:55:32 -0400

    Changed scrollview to flatlist

--- academind-react-native/App.js
+++ academind-react-native/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -24,13 +24,14 @@ export default function App() {
         />
         <Button title="ADD" onPress={addGoalHandler}/>
       </View>
-      <ScrollView>
-        {courseGoals.map((goal) => (
-          <View key={goal} style={styles.listItem}>
-            <Text>{goal}</Text>
+      <FlatList 
+        data={courseGoals} 
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item}</Text>
           </View>
-        ))}
-      </ScrollView>
+        )} 
+      />
     </View>
   );
 }
