34be5df14f8994eb880ab009d62c76204ab5edcd
DeletedC <32579485+DeletedC@users.noreply.github.com>
2021-11-15 10:47:34 -0500

    Created 404 route and page

--- react-router-tutorial/src/App.js
+++ react-router-tutorial/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
